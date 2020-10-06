76776a1bf6a1b5c313c70c52a661ff4e9d35ba25
dermatou <mat@koala.ch>
2020-10-06 08:03:30 +0200

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   'exchange' : {
     'username'  : process.env.USERNAME || 'SVCACCT_EMAIL@DOMAIN.COM',
     'password'  : process.env.PASSWORD || 'PASSWORD',
-    'uri'       : 'https://exchange.evok.ch/EWS/Exchange.asmx'
+    'uri'       : 'https://outlook.com/ews/exchange.asmx'
   },
   // Ex: CONTOSO.COM, Contoso.com, Contoso.co.uk, etc.
   'domain' : process.env.DOMAIN || 'DOMAIN.COM'
